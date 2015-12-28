9e6c7b92dff8d4fcff4bbb5a89e17d91ae1a8634
Christian Nikkanen <christian.nikkanen@gmail.com>
2015-12-28 13:02:40 +0200

    added callback possibility and made it more wp friendly

--- link2wp.class.php
+++ link2wp.class.php
@@ -4,190 +4,222 @@ require "vendor/autoload.php";
 
 class Link2WP {
 
-	protected static $debug_mode;
+  protected static $debug_mode;
 
-	/**
-	 * Determine if code should run in "debug" mode.
-	 * @param type|bool $debug_mode
-	 * @return type
-	 */
+  /**
+   * Determine if code should run in "debug" mode.
+   * @param type|bool $debug_mode
+   * @return type
+   */
 
-	function __construct($debug_mode = false){
-		if($debug_mode){
-			self::$debug_mode = true;
-		}
-		else{
-			self::$debug_mode = false;
-		}
-	}
+  function __construct($debug_mode = false){
+    if($debug_mode){
+      self::$debug_mode = true;
+    }
+    else{
+      self::$debug_mode = false;
+    }
+  }
 
-	/**
-	 * Parse page from string, ie. cURL result (or file_get_contents)
-	 * @param type $page_contents
-	 * @param type|string $bodypart
-	 * @param type|string $titlepart
-	 * @return type
-	 */
+  /**
+   * Parse page from string, ie. cURL result (or file_get_contents)
+   * @param type $page_contents
+   * @param type|string $bodypart
+   * @param type|string $titlepart
+   * @return type
+   */
 
-	public function parsePage($page_contents,$bodypart = "body", $titlepart = 'title'){
+  public function parsePage($page_contents,$bodypart = "body", $titlepart = 'title'){
 
-		if(is_null($titlepart)) $titlepart = 'title';
+    if(is_null($titlepart)) $titlepart = 'title';
 
-		$crawler = new Symfony\Component\DomCrawler\Crawler($page_contents);
+    $crawler = new Symfony\Component\DomCrawler\Crawler($page_contents);
 
-		$parsed_page = new stdClass();
+    $parsed_page = new stdClass();
 
-		$parsed_page->title = $crawler->filter($titlepart)->eq(0)->text();
+    $parsed_page->title = $crawler->filter($titlepart)->eq(0)->text();
 
-		$crawler = $crawler->filter($bodypart);
-		foreach ($crawler as $domElement) {
-				$parsed_page->content = $domElement->nodeValue;
-		}
+    $crawler = $crawler->filter($bodypart);
+    $parsed_page->content = $crawler->html();
 
-		if(self::$debug_mode){
-			echo "Parsed page:".PHP_EOL;
-			print_r($parsed_page);
-		}
+    if(self::$debug_mode){
+      echo "Parsed page:".PHP_EOL;
+      print_r($parsed_page);
+    }
 
-		return $parsed_page;
+    return $parsed_page;
 
-	}
+  }
 
-	/**
-	 * Create post to Wordpress.
-	 * @param type|string $title
-	 * @param type|string $content
-	 * @param type|array $opt
-	 * @return type
-	 */
+  public static function getComponentText($page_contents,$component_selector = null){
+    if(is_null($component_selector)) throw new Exception("Component selector cannot be null!", 1);
 
-	public function createPost($title = "(no title)", $content = "(no content)",$opt = array()){
-		$p = array(
-			"post_content" => $content,
-			"post_title" => $title,
-		); // arguments for wp_insert_post();
+    $crawler = new Symfony\Component\DomCrawler\Crawler($page_contents);
 
-		foreach($opt as $option_key => $option_value){
-			$p[$option_key] = $option_value;
-		}
+    $component = $crawler->filter($component_selector)->eq(0)->text();
 
-		if(self::$debug_mode){
-			echo "Post creation is disabled in debug mode. Arguments: ".PHP_EOL;
-			var_dump($p);
-			return;
-		}
 
-		$post_id = wp_insert_post($p);
 
-		return $post_id;
-	}
+    if(self::$debug_mode){
+      echo "Component text:".PHP_EOL;
+      print_r($component);
+    }
 
-	/**
-	 * Update post in wordpress
-	 * @param type|null $id
-	 * @param type|string $title
-	 * @param type|string $content
-	 * @param type|array $opt
-	 * @return type
-	 */
+    return $component;
+  }
 
-	public function updatePost($id = null, $title = "(no title)", $content = "(no content)", $opt = array()){
-		if(is_null($id)) throw new Exception("ID cannot be null!", 1);
+  /**
+   * Create post to Wordpress.
+   * @param type|string $title
+   * @param type|string $content
+   * @param type|array $opt
+   * @return type
+   */
 
-		$p = array(
-			"ID" => $id,
-			"post_content" => $content,
-			"post_title" => $title,
-		); // arguments for wp_insert_post();
+  public function createPost($title = "(no title)", $content = "(no content)",$opt = array()){
+    $p = array(
+      "post_content" => $content,
+      "post_title" => $title,
+    ); // arguments for wp_insert_post();
 
-		foreach($opt as $option_key => $option_value){
-			$p[$option_key] = $option_value;
-		}
+    foreach($opt as $option_key => $option_value){
+      $p[$option_key] = $option_value;
+    }
 
-		if(self::$debug_mode){
-			echo "Post updates are disabled in debug mode. Arguments: ".PHP_EOL;
-			var_dump($p);
-			return;
-		}
+    if(self::$debug_mode){
+      echo "Post creation is disabled in debug mode. Arguments: ".PHP_EOL;
+      var_dump($p);
+      return;
+    }
 
-		$post_id = wp_insert_post($p);
+    $post_id = wp_insert_post($p);
 
-		return $post_id;
-	}
+    if(function_exists("link2wp_createpost_callback")){
 
-	/**
-	 * Run the class functions.
-	 * @param type|array $links
-	 * @param type $crawl_options
-	 * @param type|bool $update
-	 * @param type|array $opt
-	 * @return type
-	 */
+      $args = array("postID" => $post_id,"misc" => $p);
+      call_user_func("link2wp_createpost_callback", $args); // possible to create post meta
+    }
 
-	public function init($links = array(), $crawl_options, $update = false, $opt = array()){
-		// update is false by default, create new posts
 
-		if(is_array($crawl_options)){
-			$bodypart = $crawl_options[0];
-			$titlepart = $crawl_options[1];
-		}
-		else{
-			$bodypart = $crawl_options;
-		}
 
-		foreach($links as $id => $link){
-			$page_contents = $this->fetchPage($link);
-			if(!$page_contents) throw new Exception("Request timed out / 404 at link: $link", 1);
+    return $post_id;
+  }
 
-			$parsed_page = $this->parsePage($page_contents,$bodypart, (isset($titlepart) ? $titlepart : null));
+  /**
+   * Update post in wordpress
+   * @param type|null $id
+   * @param type|string $title
+   * @param type|string $content
+   * @param type|array $opt
+   * @return type
+   */
 
-			if($update){
-				$this->updatePost($id,$parsed_page->title,$parsed_page->content,$opt);
-			}
+  public function updatePost($id = null, $title = "(no title)", $content = "(no content)", $opt = array()){
+    if(is_null($id)) throw new Exception("ID cannot be null!", 1);
 
-			else{
-				$this->createPost($parsed_page->title,$parsed_page->content,$opt);
-			}
-		}
+    $p = array(
+      "ID" => $id,
+      "post_content" => $content,
+      "post_title" => $title,
+    ); // arguments for wp_insert_post();
 
-		if(self::$debug_mode){
-			echo "Links array: ".PHP_EOL;
-			var_dump($links);
+    foreach($opt as $option_key => $option_value){
+      $p[$option_key] = $option_value;
+    }
 
-			echo "Crawl options: ".PHP_EOL;
-			var_dump($crawl_options);
+    if(self::$debug_mode){
+      echo "Post updates are disabled in debug mode. Arguments: ".PHP_EOL;
+      var_dump($p);
+      return;
+    }
 
-			echo "Is it update mode?: ".PHP_EOL;
-			var_dump($update);
+    $post_id = wp_insert_post($p);
 
-			echo "Optional arguments: ".PHP_EOL;
-			var_dump($opt);
-		}
+    if(function_exists("link2wp_updatepost_callback")){
+      call_user_func_array("link2wp_updatepost_callback", array($post_id,$p)); // possible to create post meta
+    }
 
-	}
 
-	/**
-	 * Fetches page, timeout is 10 seconds
-	 * @param type $url
-	 * @param type $timeout
-	 * @return type
-	 */
 
-	public function fetchPage($url,$timeout = 10){
+    return $post_id;
+  }
 
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_AUTOREFERER, TRUE);
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+  /**
+   * Run the class functions.
+   * @param type|array $links
+   * @param type $crawl_options
+   * @param type|bool $update
+   * @param type|array $opt
+   * @return type
+   */
 
-		$data = curl_exec($ch);
-		curl_close($ch);
+  public function init($links = array(), $crawl_options, $update = false, $opt = array()){
+    // update is false by default, create new posts
 
-		return $data;
-	}
+    if(is_array($crawl_options)){
+      $bodypart = $crawl_options[0];
+      $titlepart = $crawl_options[1];
+    }
+    else{
+      $bodypart = $crawl_options;
+    }
+
+    foreach($links as $id => $link){
+      $page_contents = self::fetchPage($link);
+      if(!$page_contents) throw new Exception("Request timed out / 404 at link: $link", 1);
+
+      $parsed_page = $this->parsePage($page_contents,$bodypart, (isset($titlepart) ? $titlepart : null));
+
+      $opt['link2wp_original_link'] = $link;
+
+
+      if($update){
+        $this->updatePost($id,$parsed_page->title,$parsed_page->content,$opt);
+      }
+
+      else{
+        $this->createPost($parsed_page->title,$parsed_page->content,$opt);
+      }
+    }
+
+    if(self::$debug_mode){
+      echo "Links array: ".PHP_EOL;
+      var_dump($links);
+
+      echo "Crawl options: ".PHP_EOL;
+      var_dump($crawl_options);
+
+      echo "Is it update mode?: ".PHP_EOL;
+      var_dump($update);
+
+      echo "Optional arguments: ".PHP_EOL;
+      var_dump($opt);
+    }
+
+  }
+
+  /**
+   * Fetches page, timeout is 10 seconds
+   * @param type $url
+   * @param type $timeout
+   * @return type
+   */
+
+  public static function fetchPage($url,$timeout = 10){
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_AUTOREFERER, TRUE);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+
+    $data = curl_exec($ch);
+    curl_close($ch);
+
+    return $data;
+  }
 
 
 }
